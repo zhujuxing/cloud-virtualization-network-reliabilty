a6dc6cf8f7eddc70bc39bf9c29f118036c3203a3
zhujuxing <zhujuxing@foxmail.com>
2021-01-25 15:17:09 +0800

    测试报告v1

--- src/AppAvaCal.py
+++ src/AppAvaCal.py
@@ -48,7 +48,7 @@ def app_ava_cal(file,T,N):
         g_T= NetEvoRulAna.net_evo_rul_ana_test(g_T,evol) # 修改net_evo_rul_ana_test为正式版函数名
         single_app_avail[i+1] = g_T.graph['Application_info']['ApplicationDownTime'].apply(lambda x:1-(x/(T*365*24)))
         g_T.displayApp()
-        NetEvoRulAna.saveLog()
+        # NetEvoRulAna.saveLog()
     NetEvoRulAna.clearVar()
 
     single_app_avail['result'] = single_app_avail.apply(np.mean, axis=1)
@@ -62,14 +62,35 @@ def app_ava_cal(file,T,N):
     return single_app_avail, whole_app_avail
     
 
-def test():
-    T = 100
-    N = 10
+def test_T():
+    N = 100
     file = os.path.abspath(os.path.dirname(os.getcwd())+os.path.sep+".")+os.sep+'test'+os.sep+'file.xlsx'
-    return app_ava_cal(file, T, N)
+    result = pd.DataFrame()
+    for T in [10,20,50,100,200]:
+        t1 = time.time()
+        single_app_avail, whole_app_avail = app_ava_cal(file, T, N)
+        t2 = time.time()
+        result_i = single_app_avail['result']
+        result_i['whole'] = whole_app_avail
+        result_i['time'] = t2-t1
+        result[T] = result_i
+    # print("总用时：",round(t2-t1, 3),"秒")
+    return result
+
+def test_N():
+    T = 200
+    file = os.path.abspath(os.path.dirname(os.getcwd())+os.path.sep+".")+os.sep+'test'+os.sep+'file.xlsx'
+    result = pd.DataFrame()
+    for N in [10,50,100,200,500]:
+        t1 = time.time()
+        single_app_avail, whole_app_avail = app_ava_cal(file, T, N)
+        t2 = time.time()
+        result_i = single_app_avail['result']
+        result_i['whole'] = whole_app_avail
+        result_i['time'] = t2-t1
+        result[N] = result_i
+    return result
 
 if __name__ == '__main__':
-    t1 = time.time()
-    single_app_avail, whole_app_avail = test()
-    t2 = time.time()
-    print("总用时：",round(t2-t1, 3),"秒")
+    result_T = test_T()
+    result_N = test_N()
